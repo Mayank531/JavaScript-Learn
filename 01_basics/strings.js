bd8fe1ae7efb3ce543a52c19a7d82cc22f5f4cf4
Yash <150576761+Mayank531@users.noreply.github.com>
2025-08-08 16:40:50 +0000

    Adding the Array file

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,32 +1,35 @@
 fname = "Viraj";
-lname= "Gaikwad";
+// lname= "Gaikwad";
 
-console.log(`My name is ${fname} ${lname}`);
+// console.log(`My name is ${fname} ${lname}`);
 
-const myName = new String("BahbadburShyam ")
+// const myName = new String("BahbadburShyam ")
 
-console.log(myName[8]);
-console.log(myName.__proto__)
+// console.log(myName[8]);
+// console.log(myName.__proto__)
 
-console.log(myName.length);
-console.log(myName.toUpperCase());
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
 
-console.log(myName.indexOf('b'));
-console.log(myName.substring(0,7));
-console.log(myName.slice(-8,9 ));
+// console.log(myName.indexOf('b'));
+// console.log(myName.substring(0,7));
+// console.log(myName.slice(-8,9 ));
 
-const newName = "    Chetan    "
-console.log(newName);
-console.log(newName.trim())
+// const newName = "    Chetan    "
+// console.log(newName);
+// console.log(newName.trim())
 
-// Replace
+// // Replace
 
-const email = "rajshamani@gmail.com";
-console.log(email.replace("@","-"));
-console.log(email.includes("rAj"));
+// const email = "rajshamani@gmail.com";
+// console.log(email.replace("@","-"));
+// console.log(email.includes("rAj"));
 
-console.log(email.split("@"))
+// console.log(email.split("@"))
 
-console.log(email.repeat(3));
+// console.log(email.repeat(3));
+
+console.log(fname[0]);
+
 
 
---
+++ 02_basics_objects/arrays.js
@@ -0,0 +1,46 @@
+arr1=[1,2,3]
+arr2=[4,5,6]
+
+// Array Methods =>
+
+
+//ans=arr1.concat(arr2)
+// console.log(ans);
+// console.log(typeof ans);
+
+
+// ans.push(10)
+// console.log(ans);
+
+// ans.pop();
+// console.log(ans)
+
+// ans.unshift(8) // Adds element at the start
+// console.log(ans);
+
+// ans.shift();
+// console.log(ans);  // Removes the first element
+
+// console.log(ans.includes(3));
+// console.log(ans.includes(10));
+
+// let myNewArr = ans.join(" ");
+// console.log(myNewArr);
+
+//ans = [...arr1,...arr2]
+//ans = Array.of(7,990,3,2,4,55,5,4,2,2)
+//console.log(ans);
+
+// Flat =>  combines the array in an array in an array ....... into a single array
+
+// nested_array = [1,2,3,[1,4,9],4,5,[1,[8,18],90]]
+// console.log(nested_array.flat(Infinity))
+
+
+
+
+
+
+
+
+
