20e395a155d65f1f59ca5228cddee69627036b88
Yash <150576761+Mayank531@users.noreply.github.com>
2025-08-08 07:42:33 +0000

    Adding String file of JS code

---
+++ 01_basics/strings.js
@@ -0,0 +1,32 @@
+fname = "Viraj";
+lname= "Gaikwad";
+
+console.log(`My name is ${fname} ${lname}`);
+
+const myName = new String("BahbadburShyam ")
+
+console.log(myName[8]);
+console.log(myName.__proto__)
+
+console.log(myName.length);
+console.log(myName.toUpperCase());
+
+console.log(myName.indexOf('b'));
+console.log(myName.substring(0,7));
+console.log(myName.slice(-8,9 ));
+
+const newName = "    Chetan    "
+console.log(newName);
+console.log(newName.trim())
+
+// Replace
+
+const email = "rajshamani@gmail.com";
+console.log(email.replace("@","-"));
+console.log(email.includes("rAj"));
+
+console.log(email.split("@"))
+
+console.log(email.repeat(3));
+
+
