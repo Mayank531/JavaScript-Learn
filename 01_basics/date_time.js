e716bf4ac31b98ec1ad1d5aaff3cca2b087be329
Yash <150576761+Mayank531@users.noreply.github.com>
2025-08-08 13:44:51 +0000

    Added date_time file

---
+++ 01_basics/date_time.js
@@ -0,0 +1,29 @@
+const date = new Date();
+// console.log(date);
+
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+
+// console.log(date.toJSON());
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.toLocaleString('default',{
+    dateStyle:"full"
+}))
+
+
+// let myDate = new Date(2023,1,22,14,30,55)
+// // console.log(myDate.toDateString());
+// // console.log(myDate.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp.toLocaleString());
+// console.log(myDate.getTime());
+
+
+
+
---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,16 @@
+// amount = 10000000.63;
+
+// console.log(amount.toFixed(2));
+
+// console.log(amount.toPrecision(8));
+
+// console.log(amount.toLocaleString("en-IN"));
+
+// -----=============== Maths 
+
+console.log(Math.abs(-45));
+console.log(Math.random());
+min = 10;
+max=50;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+ 
