b11fb67840b50d19f9586b095750b56de17eee19
Yash <150576761+Mayank531@users.noreply.github.com>
2025-08-08 05:00:12 +0000

    Adding Conversion and Comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>0);
+
+
+// console.log("2"<0);
+
+// console.log(true>false);
+
+// console.log(null>0);            // the comparison operator(>,<,>=,<=) convert the null into a number treating it as i.e 0
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);            
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+console.log(2 >true);
+
+
+// === is a strict equality operator. It checks the value as well as the data type unlike == equality operator
+
+console.log("3"===3);
+console.log(3===3);
---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,19 @@
+score=3;   
+
+negScore = -score;
+
+console.log(negScore);    // -3
+console.log(2+2);         // 4
+
+console.log(2 + 3 + "3"+ 3 + 3);      // 5333
+
+console.log("3" + 3);                 // =>  33
+console.log("3" * 3 + "1");           // =>  91
+console.log("3" / 3);                 //=>   1
+
+console.log(+true);                //=>    1
+console.log(+"");                  //=>    0
+
+console.log(score++);
+
+
