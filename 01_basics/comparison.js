44b4d11f9acc9b7992627d6b74b33686231e4d6e
Yash <150576761+Mayank531@users.noreply.github.com>
2025-08-08 05:12:54 +0000

    datatypes

--- 01_basics/comparison.js
+++ 01_basics/comparison.js
@@ -20,3 +20,5 @@ console.log(2 >true);
 
 console.log("3"===3);
 console.log(3===3);
+
+
--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -5,3 +5,16 @@ firstName="Ankur"
 isLiked = true;
 
 console.table([typeof firstName, typeof isLiked, typeof null, typeof undefined])
+
+array1  = [1,2]
+myObj={
+    age:12,
+    fname:"yyu"
+}
+fun= function(){
+    console.log("hello");
+    
+}
+console.log(typeof(ar));
+console.log(typeof(ob));
+console.log(typeof(fun));
