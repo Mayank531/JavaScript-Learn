ee4fd0b9134ce9dc0500f3cbcc7d1fb165735697
Yash <150576761+Mayank531@users.noreply.github.com>
2025-08-09 10:04:55 +0000

    functions file added

---
+++ 03_basics/functions.js
@@ -0,0 +1,14 @@
+// function sayHello(){
+//     console.log("Hello World!");
+    
+// }
+
+// sayHello()
+
+function add(num1,num2){
+    console.log(num1+num2)
+    return num1+num2
+}
+
+ans = add(1,3)
+console.log(`Anser is ${ans}`)
